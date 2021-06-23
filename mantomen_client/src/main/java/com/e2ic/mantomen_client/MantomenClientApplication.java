80e71dac57c13c71d16f1518232b58487831eb38
smdkrak <sm-dkrak@hanmail.net>
2021-06-24 00:03:41 +0900

    서버, 클라이언트 프로젝트 생성

---
+++ mantomen_client/src/main/java/com/e2ic/mantomen_client/MantomenClientApplication.java
@@ -0,0 +1,13 @@
+package com.e2ic.mantomen_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantomenClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantomenClientApplication.class, args);
+	}
+
+}
---
+++ mantomen_client/src/test/java/com/e2ic/mantomen_client/MantomenClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e2ic.mantomen_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantomenClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
